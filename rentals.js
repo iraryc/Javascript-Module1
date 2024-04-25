8a48429378858e6cdc719acae87612090c30b983
Andrea Ceballos <iraryc@gmail.com>
2024-04-25 09:21:48 -0700

    Update rentals.js

--- rentals.js
+++ rentals.js
@@ -6,8 +6,14 @@ let vancost = 250;
 
 let totalVansNeeded= people / vanpeople;
 
-let totalvancost = vancost * totalVansNeeded;
+let totalvancost = vancost * Math.ceil(totalVansNeeded);
+
+let amoutperperson = totalvancost / people
 
 
 
 console.log(Math.ceil(totalVansNeeded));
+
+console.log(totalvancost.toFixed(2));
+
+console.log(`The amount per person would be $${amoutperperson.toFixed(2)}`);
